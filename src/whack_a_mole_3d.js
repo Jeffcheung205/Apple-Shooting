d0d8d538cacdcc0fb1f9aa2c12322d1a57d87814
Jeffcheung205 <disneyland6029@gmail.com>
2024-08-10 11:37:17 +0800

    Update whack_a_mole_3d.js

--- src/whack_a_mole_3d.js
+++ src/whack_a_mole_3d.js
@@ -1,3 +1,9 @@
+import { extend } from '@react-three/fiber';
+import { Text } from 'three-stdlib';
+
+
+extend({ Text });
+
 window.initGame = (React, assetsUrl) => {
   const { useState, useEffect, useRef, Suspense } = React;
   const { useFrame, useLoader, useThree } = window.ReactThreeFiber;
