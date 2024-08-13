27ddc97aa49918dde1ed6a6c6a588683eb103fdd
Jeffcheung205 <disneyland6029@gmail.com>
2024-08-13 10:26:25 +0800

    Update apple_shooting.js

--- src/apple_shooting.js
+++ src/apple_shooting.js
@@ -3,6 +3,7 @@ window.initGame = (React, assetsUrl) => {
   const { useFrame, useLoader, useThree } = window.ReactThreeFiber;
   const THREE = window.THREE;
   const { GLTFLoader } = window.THREE;
+  extend({ Text });
 
   const AppleModel = React.memo(function AppleModel({ url, scale = [1, 1, 1], position = [0, 0, 0] }) {
     const gltf = useLoader(GLTFLoader, url);
